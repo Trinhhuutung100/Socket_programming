aa067c6317728000809042849932731b2cbf1933
trinhhuutung100 <trinhhuutung100@gmail.com>
2023-04-23 21:06:07 +0700

    bai 3

--- Bai_tap_tren_lop_1/tempCodeRunnerFile.c
+++ Bai_tap_tren_lop_1/tempCodeRunnerFile.c
@@ -1,7 +1,2 @@
-p = realloc(p + strlen(p), strlen(buf));
-        memcpy(p + strlen(p), buf, strlen(buf));
-        int n=0;
-        while(p = strstr(p, "0123456789")){
-            p++;
-            n++;
-        }
+
+        fprintf(f, "%s", buf);
---
+++ Bai_tap_tren_lop_1/udp_file_sender.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(int argc, char * argv[])
+{
+    int sender = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htons(atoi(argv[2]));
+
+    char buf[256];
+    FILE *f  = fopen(argv[3], "r");
+
+    sendto(sender, argv[3], strlen(argv[3]), 0, (struct sockaddr *)&addr, sizeof(addr));
+
+    while (!feof(f))
+    {
+        fgets(buf, sizeof(buf), f);
+        int ret = sendto(sender, buf, strlen(buf), 0, (struct sockaddr *)&addr, sizeof(addr));
+        printf("buf : %s\n", buf);
+        printf("%d bytes sent.\n\n", ret);
+    }
+
+    fclose(f);
+}
