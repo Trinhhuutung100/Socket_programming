8ba29cf1b57fe414c5c35c9a5e6e0a0e4e2fa4bd
trinhhuutung100 <trinhhuutung100@gmail.com>
2023-04-24 09:39:19 +0700

    update

--- Bai_tap_tren_lop_1/multi_receiver.c
+++ Bai_tap_tren_lop_1/multi_receiver.c
@@ -18,27 +18,31 @@ int main(int argc, char * argv[])
 
     bind(receiver, (struct sockaddr *)&addr, sizeof(addr));
 
-    char buf[256];
-    char file_name[256];
     struct sockaddr_in sender_addr;
     int sender_addr_len = sizeof(sender_addr);
+    char buf[256];
+    int count = 0;
 
-    int ret = recvfrom(receiver, file_name, sizeof(file_name), 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
-    file_name[ret] = 0;
-
-    char new_name[256];
-    sprintf(new_name, "new_%s", file_name);
-    printf("ten file duoc ghi du lieu : %s\n\n", new_name);
-    FILE *f = fopen(new_name, "a");
+    FILE *f;
+    
     while (1)
     {
         int ret = recvfrom(receiver, buf, sizeof(buf), 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
         if (ret < sizeof(buf)) buf[ret] = 0;
-        if(strncmp(buf, "Close", 5) == 0) break;
+        if(strncmp(buf, "gui moi", 7)==0) {
+            count++;
+            char file_name[256];
+            int ret = recvfrom(receiver, file_name, sizeof(file_name), 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
+            file_name[ret] = 0;
+            char new_name[256];
+            sprintf(new_name, "new_%s_%d", file_name, count);
+            printf("ten file duoc ghi du lieu : %s\n\n", new_name);
+            f = fopen(new_name, "a");
+            continue;
+        }
         printf("%d bytes received %s: %s\n\n", ret, inet_ntoa(sender_addr.sin_addr), buf);
         fprintf(f, "%s", buf);
     }
 
     fclose(f);
-    close(receiver);
 }
--- Bai_tap_tren_lop_1/multi_sender.c
+++ Bai_tap_tren_lop_1/multi_sender.c
@@ -16,7 +16,10 @@ int main(int argc, char * argv[])
     addr.sin_addr.s_addr = inet_addr(argv[1]);
     addr.sin_port = htons(atoi(argv[2]));
 
+    char sen[]= "gui moi";
     char buf[256];
+
+    sendto(sender, sen, strlen(sen), 0, (struct sockaddr *)&addr, sizeof(addr));
     FILE *f  = fopen(argv[3], "r");
 
     sendto(sender, argv[3], strlen(argv[3]), 0, (struct sockaddr *)&addr, sizeof(addr));
@@ -24,13 +27,11 @@ int main(int argc, char * argv[])
     while (!feof(f))
     {
         fgets(buf, sizeof(buf), f);
+        sleep(5);
         int ret = sendto(sender, buf, strlen(buf), 0, (struct sockaddr *)&addr, sizeof(addr));
         printf("buf : %s\n", buf);
         printf("%d bytes sent.\n\n", ret);
     }
-
-    char closefile[] ="Close";
-    sendto(sender, closefile, strlen(closefile), 0, (struct sockaddr *)&addr, sizeof(addr));
 
     fclose(f);
 }
